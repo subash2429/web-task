3e9da262a5bb62222f9a1be35ff2113d19ae8a67
subash2429 <subashgoutham2429@gmail.com>
2025-11-29 13:21:13 +0530

    connection

--- backend/server.js
+++ backend/server.js
@@ -9,7 +9,7 @@ import { errorHandler } from './middleware/errorHandler.js';
 dotenv.config();
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 10000;
 
 
 app.use(cors({
