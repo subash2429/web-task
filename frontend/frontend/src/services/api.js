16a1fefa3154bff3921a8f22c17765277290e0bd
subash2429 <subashgoutham2429@gmail.com>
2025-11-29 13:14:07 +0530

    connection

--- frontend/frontend/src/services/api.js
+++ frontend/frontend/src/services/api.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 
-const API_BASE_URL = import.meta.env.VITE_API_URL || 'http://localhost:4000/api';
+const API_BASE_URL = "https://web-task-ot6h.onrender.com";
+
 
 const api = axios.create({
   baseURL: API_BASE_URL,
