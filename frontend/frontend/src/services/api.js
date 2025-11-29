9c37b63dfa872db719a6ccbd573f64e8ee1458b3
subash2429 <subashgoutham2429@gmail.com>
2025-11-29 16:37:26 +0530

    connection

--- frontend/frontend/src/services/api.js
+++ frontend/frontend/src/services/api.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 
-const API_BASE_URL = "https://web-task-ot6h.onrender.com/api";
+const API_BASE_URL = "https://web-task-ot6h.onrender.com/api" ;
+ //const API_BASE_URL = "http://localhost:10000/api" ;
 
 
 const api = axios.create({
@@ -17,38 +18,38 @@ export const getAllItems = async () => {
 };
 
 export const getItem = async (id) => {
-  const response = await api.get(`/inventory/${id}`);
+  const response = await api.get(`/inventory${id}`);
   return response.data.data;
 };
 
 export const addItem = async (itemData) => {
-  const response = await api.post('/', itemData);
+  const response = await api.post('/inventory/', itemData);
   return response.data.data;
 };
 
 export const updateItem = async (id, itemData) => {
-  const response = await api.put(`/${id}`, itemData);
+  const response = await api.put(`/inventory/${id}`, itemData);
   return response.data.data;
 };
 
 export const deleteItem = async (id) => {
-  const response = await api.delete(`/${id}`);
+  const response = await api.delete(`/inventory/${id}`);
   return response.data;
 };
 
 export const updateMultipleItems = async (items) => {
-  const response = await api.put('/bulk/update', { items });
+  const response = await api.put('/inventory/bulk/update', { items });
   return response.data.data;
 };
 
 
 export const getAllCoupons = async () => {
-  const response = await api.get('/');
+  const response = await api.get('/coupons/');
   return response.data.data;
 };
 
 export const validateCoupon = async (code) => {
-  const response = await api.post('/validate', { code });
+  const response = await api.post('/coupons/validate', { code });
   return response.data.data;
 };
 
